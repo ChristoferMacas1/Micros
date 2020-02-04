be2b7f61dd62e2219ebb92e0ba26ef107d56a30d
ChristoferMacas1 <49549377+ChristoferMacas1@users.noreply.github.com>
2020-02-04 12:47:03 -0500

    Add files via upload

---
+++ deberPower.ino
@@ -0,0 +1,82 @@
+/*
+   UNIVERSIDAD TECNICA DEL NORTE
+   FICA CIERCOM
+   CHRISTOFER MACAS
+   Realizar un programa que cada 5 segundos el sistema desactiva el conversor análogo digital,
+   cada 8 segundos la comunicación serial y cada 12 segundos un desactivación total.
+   Esta información debe verse en una LCD. Suba su enlace de su cuenta GitHub.
+*/
+
+#include <LowPower.h>
+#include <avr/wdt.h>
+#include <EEPROM.h>
+#include<avr/sleep.h>
+#include <MsTimer2.h>
+#include <TimerOne.h>
+#include <avr/power.h>
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+int cont = 0;
+int dato;
+int pLcd = 7;
+int num = EEPROM.read(0);
+
+void setup() {
+  Timer1.initialize(1000000);
+  Timer1.attachInterrupt(conteo);
+  power_all_enable();
+  lcd.begin(16, 2);
+  num = 0;
+}
+
+void loop() {
+  Serial.println(num);
+  dormir();
+  lcd.setCursor(6, 1);
+  lcd.print("Tiempo:" + String(cont));
+  dato = analogRead(0);
+  lcd.setCursor(0, 1);
+  lcd.print(dato);
+}
+void conteo() {
+  cont++;
+}
+
+void dormir() {
+  if (cont == 5) {
+    num = num + 1;
+    lcd.setCursor(0, 0);
+    lcd.print("CAD DESACTIVADO");
+    power_adc_disable();
+
+  }
+  if (cont == 8) {
+    lcd.setCursor(0, 0);
+    lcd.print("                     ");
+    lcd.setCursor(0, 0);
+    lcd.print("CX DESACTIVADO");
+    power_spi_disable();
+  }
+  if (cont == 12) {
+    lcd.setCursor(0, 0);
+    lcd.print("                    ");
+    lcd.setCursor(0, 0);
+    lcd.print("DESACTIVADO");
+    power_all_disable();
+  }
+  if (EEPROM.read(0) > 12) {
+    num = 0;
+    power_all_enable();
+    EEPROM.write(0, num);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("REINICIO");
+    lcd.setCursor(0, 1);
+    lcd.print("DEL SISTEMA");
+    digitalWrite(pLcd, HIGH);
+    delay(10);
+    wdt_enable(WDTO_1S);
+
+  }
+}
